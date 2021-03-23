bd3f38beb474b737eaa9553da039ab5294c802a5
Alessandro Brachetti <alebrack13@gmail.com>
2021-03-23 15:20:50 +0100

    Esercizio Completo + Bonus

---
+++ main.js
@@ -0,0 +1,12 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data
+//Aggiungere alla pagina un’immagine, presa anch’essa da un data
+
+var app = new Vue (
+  {
+    el:'#root',
+    data: {
+      message: 'Hello, I am a rubber duck!',
+      image: 'https://www.bakaji.com/images/prodotti/101020184029811346011_1.jpg',
+    }
+  }
+);
